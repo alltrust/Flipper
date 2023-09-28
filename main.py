89782d2f86a7f71c218415dbd131f94e8f910b4d
alltrust <aldorgarcia29@gmail.com>
2023-09-28 10:15:52 -0400

    ui and functionality

--- main.py
+++ main.py
@@ -1,36 +1,17 @@
 import pandas as pd
 import tkinter
 import random
+from PIL import Image, ImageTk
 
-CARD_BCG = "#33BBC5"
-WRONG_BTN = "#FF6969"
-RIGHT_BTN = "#85E6C5"
 MAIN_BCG = "#614BC3"
-FONT = ("Courier", 38)
+FONT = ("Courier", 22)
 initial_start = True
 original_word = None
 translation = None
 
 language_file = "freq_span.csv"
 translations_df = pd.read_csv(language_file)
-lang1, lang2 = translations_df.columns
 
-# function to display word label with timer
-# on GUI open, don't start timer, until x or check is clicked
-# then the timer counts down until 0, which it then displays the translation
-# user then clicks 'x' or 'y' to display the next word and restart the timer
-
-
-
-def count_down(time=3):
-    """Counts down the timer and displays the """
-
-    countdownLabel["text"] = time
-
-    if time > 0 :
-        mainWindow.after(1000, count_down, time-1)
-    elif time == 0:
-        display_back()
 
 def get_words():
     """Returns the word and its translation."""
@@ -43,31 +24,41 @@ def get_words():
     original_word = word
     translation = translation
 
-# def get_translation_word():
-#     """Returns the translation of the origin"""
 
 def display_front():
     """Displays the front of the card- which is the original word."""
-    get_words()
-    word_label.config(text=original_word)
-    cardCanvas.config(background=MAIN_BCG)
+    global timer
+    mainWindow.after_cancel(timer)
 
-    # if not initial_start:
-    count_down()
+    get_words()
+ 
+    cardCanvas.itemconfig(card_title, text="English", fill="black")
+    cardCanvas.itemconfig(card_word, text=original_word, fill="black")
+    cardCanvas.itemconfig(card_side, image= new_front)
+    timer = mainWindow.after(3000,display_back)
+
+
 
 
 
 def display_back():
     """Displays the back of the card-which is the translated word"""
-    
-    word_label.config(text=translation)
-    cardCanvas.config(background=CARD_BCG)
+  
+    cardCanvas.itemconfig(card_title, text="Spanish", fill= "white")
+    cardCanvas.itemconfig(card_word, text=translation, fill="white")
+    cardCanvas.itemconfig(card_side, image= new_back)
+
     
 
 
 mainWindow = tkinter.Tk()
 mainWindow.config(background=MAIN_BCG)
-mainWindow.title("Language Learner")
+mainWindow.title("Flipper")
+mainWindow["pady"] = 18
+
+timer= "3"
+
+
 mainWindow.minsize(width=600, height=500)
 mainWindow.rowconfigure(0, weight=1)
 mainWindow.rowconfigure(1, weight=1)
@@ -77,30 +68,36 @@ mainWindow.columnconfigure(0, weight=1)
 mainWindow.columnconfigure(1, weight=1)
 mainWindow.columnconfigure(2, weight=1)
 
-cardCanvas = tkinter.Canvas(mainWindow, width=300,
-                            height=200, background=CARD_BCG)
+height = 300
+width = 400
+
+front_img = Image.open("images/card_front.png")
+resized_front_img = front_img.resize((width, height))
+new_front = ImageTk.PhotoImage(resized_front_img)
+
+back_img= Image.open("images/card_back.png")
+resized_back_img = back_img.resize((width,height))
+new_back = ImageTk.PhotoImage(resized_back_img)
+
+
+cardCanvas = tkinter.Canvas(mainWindow, width=500,
+                            height=350, background=MAIN_BCG, highlightthickness=0)
+card_side = cardCanvas.create_image(250,175,image=new_front)
+card_title = cardCanvas.create_text(250, 100,text="HELLO", font=FONT, fill="black")
+card_word = cardCanvas.create_text(250, 175, text="Click to start", fill="black", font=("Courier", 38, "bold"))
 cardCanvas.grid(row=1, column=1)
-wordLabel_num = cardCanvas.create_text(150, 100, font=FONT)
 
 buttonFrame = tkinter.Frame(mainWindow, background=MAIN_BCG)
 buttonFrame.grid(row=2, column=1, sticky="ews")
 
-countdownLabel = tkinter.Label(mainWindow, font=FONT)
-countdownLabel.grid(row=0, column=2) 
-
-word_label = tkinter.Label(mainWindow, font=FONT)
-word_label.grid(row=0, column=3)
-
-
 photoimgWrong = tkinter.PhotoImage(file="images/wrong.png")
 buttonWrong = tkinter.Button(buttonFrame, image=photoimgWrong,
-                             highlightthickness=0, command=display_front)
+                             highlightthickness=0, command=display_front, background=MAIN_BCG)
 buttonWrong.pack(side="left")
 
 photoimgRight = tkinter.PhotoImage(file="images/right.png")
 buttonRight = tkinter.Button(buttonFrame, image=photoimgRight,
-                             highlightthickness=0, command=display_front)
+                             highlightthickness=0, command=display_front, background=MAIN_BCG)
 buttonRight.pack(side="right")
-
 
 mainWindow.mainloop()
